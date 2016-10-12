90b29d53b6d4a779cf85c629e554d772461f6bc1
Ben Foxall <ben@pusher.com>
2016-10-12 08:48:53 +0100

    ball

--- 10/lib/main.js
+++ 10/lib/main.js
@@ -2,18 +2,28 @@ import Renderer from './Renderer.js'
 import Pose from './Pose.js'
 import {loop, rnd} from './util.js'
 import Thing from './Things/Thing.js'
-import Poly from './Things/Poly.js'
-import DSphere from './Things/DSphere.js'
-import DFloor from './Things/DFloor.js'
+import Ball from './Things/Ball.js'
 
 const renderer = new Renderer()
 const pose = new Pose()
 
 const world = new Thing()
 
-const floor = new DFloor(2)
-world.add(floor)
-floor.color = 'rgba(255,255,255,0.7)'
+const ball = new Ball(0,0,0,1)
+world.add(ball)
+
+
+for (var i = 0; i < 8; i++) {
+  var b = new Ball( rnd(1.5), rnd(1.5), rnd(1.5), .2 )
+
+  b.fill = 'rgba(255,255,255,0.3)'
+
+  world.add(b
+    )
+}
+
+
+ball.fill = 'rgba(255,255,255,0.3)'
 
 
 pose.on('change', transform => {
